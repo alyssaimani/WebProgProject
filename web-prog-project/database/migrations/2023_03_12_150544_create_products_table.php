7b93ee517ba382acfdcd0c52157dc5e35dd2d751
Alyssa Imani <alyssaimani44@gmail.com>
2023-03-12 23:30:19 +0700

    create migration

--- web-prog-project/database/migrations/2023_03_12_150544_create_products_table.php
+++ web-prog-project/database/migrations/2023_03_12_150544_create_products_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+        Schema::create('products', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->double('price');
+            $table->string('description')->nullable();
+            $table->enum('type', array('Book', 'Stationary'));
+            $table->string('image');
+            $table->timestamps();
         });
     }
 
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('products');
     }
 };
