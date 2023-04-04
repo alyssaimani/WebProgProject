3000f949c12d7ca00e22545e0ba76dfff4b4277d
kareltan2 <petrus.kanisius.karel.tan@gmail.com>
2023-04-05 03:40:24 +0700

    fixing database attribute

--- web-prog-project/database/migrations/2023_03_12_161912_create_carts_table.php
+++ web-prog-project/database/migrations/2023_03_12_161912_create_carts_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('carts', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id('id');
             $table->integer('product_id')->unsigned();
             $table->foreign('product_id')->references('id')->on('products');
             $table->integer('user_id')->unsigned();
