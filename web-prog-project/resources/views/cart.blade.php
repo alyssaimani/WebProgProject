b623ae834fca1b10f58655e5d7469dc9ac3ecd16
kareltan2 <petrus.kanisius.karel.tan@gmail.com>
2023-05-21 21:07:47 +0700

    add validation due to empty cart

--- web-prog-project/resources/views/cart.blade.php
+++ web-prog-project/resources/views/cart.blade.php
@@ -8,64 +8,72 @@
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     @endif
+
     <div class="d-flex justify-content-center pt-3 pb-3">
         <div class="d-flex flex-column">
             <div class="d-flex justify-content-center">
                 <h1 class="text-white text-align-center">Cart</h1>
             </div>
             
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th scope="col"></th>
-                        <th scope="col">Product Name</th>
-                        <th scope="col">Price</th>
-                        <th scope="col">Description</th>
-                        <th scope="col">Quantity</th>
-                        <th scope="col">Total</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($items as $item)
+            @if($items->isNotEmpty())
+                <table class="table">
+                    <thead>
                         <tr>
-                            <th scope="row">
-                                <form id="delete-cart" action="{{ route('cart.destroy', $item->cartId) }}" method="POST">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button class="bi bi-x-circle-fill" type="submit" onclick="return confirm('Are you sure you want to delete this cart?')"></button>
-                                </form>
-                            </th>
-                            <td>{{ $item->productName }}</td>
-                            <td>{{ $item->productPrice }}</td>
-                            <td>{{ $item->productDescription }}</td>
-                            <td>
-                                <form method="POST" action="{{ route('cart.update', $item->cartId) }}" onsubmit="return validateQuantity(this)">
-                                    @csrf
-                                    @method('PUT')
-                                    <div class="d-flex">
-                                        <div class="input-group flex-nowrap">
-                                            <input type="number" name="quantity" class="form-control bg-secondary me-2 quantity-input" aria-label="Username" aria-describedby="addon-wrapping" style="width: 80px" value="{{ $item->quantity }}">
-                                        </div>
-                                        <button class="btn btn-outline-success bg-success text-light" type="submit"><i class="bi bi-check-circle-fill"></i></button>
-                                    </div>
-                                </form>
-                            </td>
-                            <td>{{ $item->total }}</td>
+                            <th scope="col"></th>
+                            <th scope="col">Product Name</th>
+                            <th scope="col">Price</th>
+                            <th scope="col">Description</th>
+                            <th scope="col">Quantity</th>
+                            <th scope="col">Total</th>
                         </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            <div class="d-flex justify-content-center">  
-                <form method="POST" action="{{ route('cart.checkout') }}">
-                    @csrf
-                    <div class="d-flex">
-                        <div class="input-group flex-nowrap">
-                            <input type="hidden" name="id" value="{{ auth()->user()->id }}">
-                            <button class="btn btn-success" type="submit">Checkout</button>
+                    </thead>
+                    <tbody>
+                        @foreach ($items as $item)
+                            <tr>
+                                <th scope="row">
+                                    <form id="delete-cart" action="{{ route('cart.destroy', $item->cartId) }}" method="POST">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="bi bi-x-circle-fill" type="submit" onclick="return confirm('Are you sure you want to delete this cart?')"></button>
+                                    </form>
+                                </th>
+                                <td>{{ $item->productName }}</td>
+                                <td>{{ $item->productPrice }}</td>
+                                <td>{{ $item->productDescription }}</td>
+                                <td>
+                                    <form method="POST" action="{{ route('cart.update', $item->cartId) }}" onsubmit="return validateQuantity(this)">
+                                        @csrf
+                                        @method('PUT')
+                                        <div class="d-flex">
+                                            <div class="input-group flex-nowrap">
+                                                <input type="number" name="quantity" class="form-control bg-secondary me-2 quantity-input" aria-label="Username" aria-describedby="addon-wrapping" style="width: 80px" value="{{ $item->quantity }}">
+                                            </div>
+                                            <button class="btn btn-outline-success bg-success text-light" type="submit"><i class="bi bi-check-circle-fill"></i></button>
+                                        </div>
+                                    </form>
+                                </td>
+                                <td>{{ $item->total }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+
+                <div class="d-flex justify-content-center">  
+                    <form method="POST" action="{{ route('cart.checkout') }}">
+                        @csrf
+                        <div class="d-flex">
+                            <div class="input-group flex-nowrap">
+                                <input type="hidden" name="id" value="{{ auth()->user()->id }}">
+                                <button class="btn btn-success" type="submit">Checkout</button>
+                            </div>
                         </div>
-                    </div>
-                </form>                           
-            </div>            
+                    </form>                           
+                </div>       
+            @else
+                <div class="d-flex justify-content-center">
+                    <h2 class="text-white text-align-center">Your cart is empty!</h2>
+                </div> 
+            @endif   
         </div>
     </div>
 
