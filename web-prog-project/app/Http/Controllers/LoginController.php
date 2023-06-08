4c807cf37bd7f92481257f769fd42eb1d7a31516
Alyssa Imani <alyssaimani44@gmail.com>
2023-06-08 18:34:46 +0700

    fix format code

--- web-prog-project/app/Http/Controllers/LoginController.php
+++ web-prog-project/app/Http/Controllers/LoginController.php
@@ -10,8 +10,8 @@ class LoginController extends Controller
 {
     public function open(Request $request)
     {
-        $email =session(["email" => $request->email]);
-        return view('login',[
+        $email = session(["email" => $request->email]);
+        return view('login', [
             'title' => 'Login',
             'email' => $email
         ]);
@@ -28,14 +28,11 @@ class LoginController extends Controller
             Cookie::queue(Cookie::make('remember', true, 120));
             Cookie::queue(Cookie::make('email', $request->email, 120));
             Cookie::queue(Cookie::make('password', $request->password, 120));
-        }
-        
-        else {
+        } else {
             Cookie::queue(Cookie::forget('remember'));
         }
-        
-        if(Auth::attempt($credentials))
-        {
+
+        if (Auth::attempt($credentials)) {
             $request->session()->regenerate();
 
             return redirect()->intended('/');
